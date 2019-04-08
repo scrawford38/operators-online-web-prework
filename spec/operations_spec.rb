48990ef1e55fffb64458b46e52c5081794b4f4bf
Sally Crawford <sally.crawford@gmail.com>
2019-04-08 21:14:27 +0000

    Automatically backed up by Learn

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -1,4 +1,4 @@
-`require_relative './spec_helper.rb'`
+require_relative './spec_helper.rb'
 
 describe "Operations" do
   let(:file_contents) { File.read(File.join(File.dirname(__FILE__), '..', 'lib/operations.rb')) }
